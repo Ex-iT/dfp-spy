0d5d0c6fe72b55eb73d26633c18715ab819ea421
Ex-iT <vic.toothpick@gmail.com>
2020-09-06 15:19:28 +0200

    Small refactor

--- background.js
+++ background.js
@@ -1,16 +1,26 @@
 'use strict';
 
-let spy = true; // @TODO off by default
-updateBrowserAction();
-
 const spyUrls = ['*://*.doubleclick.net/gampad/ads*'];
+
+let spy = true; // @TODO off by default
+let tabId = null;
+const webRequest = chrome.webRequest;
+updateBrowserAction();
 
 // Handle icon click
 chrome.browserAction.onClicked.addListener(tab => {
 	spy = !spy;
+	tabId = tab.id;
 	updateBrowserAction();
 	chrome.tabs.update(tab.id, { url: tab.url, selected: tab.selected }, null);
+	webRequest && webRequest.handlerBehaviorChanged();
 });
+
+// onCompleted listener
+webRequest && webRequest.onCompleted.addListener(details => handleRequest(details, true), { urls: spyUrls });
+
+// onErrorOccurred listener
+webRequest && webRequest.onErrorOccurred.addListener(details => handleRequest(details, false), { urls: spyUrls });
 
 function updateBrowserAction() {
 	chrome.browserAction.setTitle({
@@ -22,14 +32,9 @@ function updateBrowserAction() {
 	});
 }
 
-// onCompleted listener
-chrome.webRequest.onCompleted.addListener(details => handleRequest(details, true), { urls: spyUrls });
-
-// onErrorOccurred listener
-chrome.webRequest.onErrorOccurred.addListener(details => handleRequest(details, false), { urls: spyUrls });
-
 // Handle requests and output
 function handleRequest(details, success) {
+	// if (spy && details.tabId === tabId) { // @TODO: does this fix 'Cannot access a chrome:// URL'?
 	if (spy) {
 		const url = new URL(details.url);
 		const banner = success
@@ -43,28 +48,55 @@ function handleRequest(details, success) {
 
 		chrome.tabs.executeScript({
 			code: `
-				console.group(${banner});
-				${getInterestingSettings(settings)}
-				${getAllSettings(settings)}
-				console.groupEnd(${banner});
+				${createLogGroup(banner,
+					getInterestingSettings(settings)
+					+ getAllSettings(settings)
+				)}
 			`
 		});
 	}
+}
+
+function createLogGroup(label, content) {
+	return `
+		console.group(${label});
+		${content}
+		console.groupEnd(${label});
+	`;
 }
 
 function getInterestingSettings(settings) {
 	const groupLabel = 'Interesting DFP Settings';
 	let code = `console.group('${groupLabel}');`;
 
-	// @TODO: make sense of these values
-	// iu_parts: 112233044,Dummy_CorpNL
-	// prev_iu_szs: 970x250|970x90|728x90,336x280|300x250,728x200|728x90,336x280|300x250,336x280|300x250,728x200|728x90,336x280|300x250,120x600|160x600
-	// prev_scp: pos=billboard|pos=rectangle_top|pos=leaderboard_mobile_double_listing|pos=rectangle_bottom|pos=rectangle_top|pos=leaderboard_mobile_double_listing|pos=rectangle_bottom|pos=skyscraper
-	// cust_params: Supercategory=computers&maincategory=laptops_en_ultrabooks&productgroup=laptop&country=BP
-
 	Object.keys(settings).forEach(key => {
-		if (key === 'url') {
-			code += `console.info('%c${key}%c: ${settings[key]}', 'font-weight: bold;', 'font-weight: normal;');`;
+		switch (key) {
+			case 'iu_parts':
+				const label = 'iu_parts';
+				const values = settings[key].split(',');
+				const dfpNetworkId = values[0];
+				const adUnit = values.slice(1).join('\n\t');
+
+				// @TODO: use 'createLogGroup' here
+				code += `console.group('${label}');`;
+				code += `console.info('%cdfpNetworkId: %c${dfpNetworkId}', 'font-weight: bold;', 'font-weight: normal;');`;
+				code += `console.info(\`%cadUnit: %c${adUnit}\`, 'font-weight: bold;', 'font-weight: normal;');`;
+				code += `console.groupEnd('${label}');`;
+				break;
+			case 'cust_params':
+				// cust_params: Supercategory=computers&maincategory=laptops_en_ultrabooks&productgroup=laptop&country=BP
+				// cust_params: page_section=xml
+				break;
+			case 'prev_scp':
+				// prev_scp: pos=billboard|pos=rectangle_top|pos=leaderboard_mobile_double_listing|pos=rectangle_bottom|pos=rectangle_top|pos=leaderboard_mobile_double_listing|pos=rectangle_bottom|pos=skyscraper
+				// prev_scp: amz_tam=true&_ia=0.00&_snhb=true&_snhb-aurc=0&consent_applies=eea&consent_value=full&hb_format=banner&hb_source=client&hb_size=160x600&hb_pb=0.06&hb_adid=605db2fa644141d&hb_bidder=openx|amz_tam=true&_ia=0.00&_snhb=true&_snhb-aurc=0&consent_applies=eea&consent_value=full
+				break;
+			case 'prev_iu_szs':
+				// prev_iu_szs: 970x250|970x90|728x90,336x280|300x250,728x200|728x90,336x280|300x250,336x280|300x250,728x200|728x90,336x280|300x250,120x600|160x600
+				break;
+			case 'url':
+				code += `console.info('%c${key}: %c${settings[key]}', 'font-weight: bold;', 'font-weight: normal;');`;
+				break;
 		}
 	});
 
@@ -78,7 +110,7 @@ function getAllSettings(settings) {
 	let code = `console.groupCollapsed('${groupLabelAll}');`;
 
 	Object.keys(settings).forEach(key => {
-		code += `console.info('%c${key}%c: ${settings[key]}', 'font-weight: bold;', 'font-weight: normal;');`;
+		code += `console.info('%c${key}: %c${settings[key]}', 'font-weight: bold;', 'font-weight: normal;');`;
 	});
 
 	code += `console.groupEnd('${groupLabelAll}');`;

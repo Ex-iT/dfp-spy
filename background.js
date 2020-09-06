af8576af29c9fdf8f13f9133528ae2fabe37853f
Ex-iT <vic.toothpick@gmail.com>
2020-09-06 23:01:33 +0200

    Updated settings viewing logic and added error handling

--- background.js
+++ background.js
@@ -2,15 +2,13 @@
 
 const spyUrls = ['*://*.doubleclick.net/gampad/ads*'];
 
-let spy = true; // @TODO off by default
-let tabId = null;
+let spy = false;
 const webRequest = chrome.webRequest;
 updateBrowserAction();
 
 // Handle icon click
 chrome.browserAction.onClicked.addListener(tab => {
 	spy = !spy;
-	tabId = tab.id;
 	updateBrowserAction();
 	chrome.tabs.update(tab.id, { url: tab.url, selected: tab.selected }, null);
 	webRequest && webRequest.handlerBehaviorChanged();
@@ -34,79 +32,114 @@ function updateBrowserAction() {
 
 // Handle requests and output
 function handleRequest(details, success) {
-	// if (spy && details.tabId === tabId) { // @TODO: does this fix 'Cannot access a chrome:// URL'?
 	if (spy) {
 		const url = new URL(details.url);
-		const banner = success
-			? `'%cDFP Spy - Request send', 'font-weight: bold;'`
-			: `'%cDFP Spy - Request failed', 'font-weight: bold; color: red;'`
-
 		const settings = {};
 		for (const [key, value] of url.searchParams.entries()) {
 			settings[key] = value;
 		}
 
-		chrome.tabs.executeScript({
+		const requestUrl = settings.url || '';
+		const banner = success
+			? `'%cDFP Spy - Request send (${requestUrl})', 'font-weight: bold;'`
+			: `'%cDFP Spy - Request failed (${requestUrl})', 'font-weight: bold; color: red;'`
+
+		chrome.tabs.executeScript(details.tabId, {
 			code: `
 				${createLogGroup(banner,
-					getInterestingSettings(settings)
-					+ getAllSettings(settings)
-				)}
-			`
+				getError(details, success)
+				+ getInterestingSettings(settings, success)
+				+ getAllSettings(settings)
+			)}`
+		}, event => {
+			const lastError = chrome.runtime.lastError;
+			if (lastError !== undefined) {
+				console.log({ tabId: details.tabId, event, lastError });
+			}
 		});
 	}
 }
 
-function createLogGroup(label, content) {
-	return `
-		console.group(${label});
-		${content}
-		console.groupEnd(${label});
-	`;
-}
-
-function getInterestingSettings(settings) {
-	const groupLabel = 'Interesting DFP Settings';
-	let code = `console.group('${groupLabel}');`;
+function getInterestingSettings(settings, success) {
+	let code = '';
 
 	Object.keys(settings).forEach(key => {
 		switch (key) {
 			case 'iu_parts':
-				const label = 'iu_parts';
 				const values = settings[key].split(',');
 				const dfpNetworkId = values[0];
 				const adUnit = values.slice(1).join('\n\t');
 
-				// @TODO: use 'createLogGroup' here
-				code += `console.group('${label}');`;
-				code += `console.info('%cdfpNetworkId: %c${dfpNetworkId}', 'font-weight: bold;', 'font-weight: normal;');`;
-				code += `console.info(\`%cadUnit: %c${adUnit}\`, 'font-weight: bold;', 'font-weight: normal;');`;
-				code += `console.groupEnd('${label}');`;
-				break;
-			case 'cust_params':
-				// cust_params: Supercategory=computers&maincategory=laptops_en_ultrabooks&productgroup=laptop&country=BP
-				// cust_params: page_section=xml
-				break;
-			case 'prev_scp':
-				// prev_scp: pos=billboard|pos=rectangle_top|pos=leaderboard_mobile_double_listing|pos=rectangle_bottom|pos=rectangle_top|pos=leaderboard_mobile_double_listing|pos=rectangle_bottom|pos=skyscraper
-				// prev_scp: amz_tam=true&_ia=0.00&_snhb=true&_snhb-aurc=0&consent_applies=eea&consent_value=full&hb_format=banner&hb_source=client&hb_size=160x600&hb_pb=0.06&hb_adid=605db2fa644141d&hb_bidder=openx|amz_tam=true&_ia=0.00&_snhb=true&_snhb-aurc=0&consent_applies=eea&consent_value=full
+				code += `
+					${createLogGroup(`'${key} - Ad path elements'`,
+					`console.info('%cdfpNetworkId: %c${dfpNetworkId}', 'font-weight: bold;', 'font-weight: normal;');
+					console.info(\`%cadUnit: %c${adUnit}\`, 'font-weight: bold;', 'font-weight: normal;');`,
+					true
+				)}`;
+
 				break;
 			case 'prev_iu_szs':
-				// prev_iu_szs: 970x250|970x90|728x90,336x280|300x250,728x200|728x90,336x280|300x250,336x280|300x250,728x200|728x90,336x280|300x250,120x600|160x600
+				const sizes = settings[key].replace(/,/g, ', ').split('|').join('\n');
+
+				code += `
+					${createLogGroup(`'${key} - Ad slot sizes'`,
+					`console.info(\`${sizes}\`);`,
+					true
+				)}`;
+
 				break;
-			case 'url':
-				code += `console.info('%c${key}: %c${settings[key]}', 'font-weight: bold;', 'font-weight: normal;');`;
+			case 'prev_scp':
+				let targetContent = '';
+				const groups = settings[key].split('|').filter(group => group);
+
+				groups.forEach((group, index) => {
+					for (const [key, value] of new URLSearchParams(group).entries()) {
+						targetContent += `console.info('%c${key}: %c${value}', 'font-weight: bold;', 'font-weight: normal;');`;
+					}
+					if (index !== groups.length - 1) {
+						targetContent += `console.info(\`\n\`);`;
+					}
+				});
+
+				code += `
+					${createLogGroup(`'${key} - Slot specific targeting parameters'`,
+					`${targetContent}`,
+					true
+				)}`;
+
+				break;
+			case 'cust_params':
+				const custParams = new URLSearchParams(settings[key]);
+				let custContent = '';
+
+				for (const [key, value] of custParams.entries()) {
+					custContent += `console.info('%c${key}: %c${value}', 'font-weight: bold;', 'font-weight: normal;');`;
+				}
+
+				code += `
+					${createLogGroup(`'${key} - Global targeting parameters'`,
+					`${custContent}`,
+					!success
+				)}`;
+
+				break;
+			case 'biw':
+				// Assuming 'bih' is set if we have 'biw'
+				code += `
+					${createLogGroup(`'Browser width / height'`,
+					`console.info(\`${settings['biw']}x${settings['bih']}\`);`,
+					true
+				)}`;
+
 				break;
 		}
 	});
-
-	code += `console.groupEnd('${groupLabel}');`;
 
 	return code;
 }
 
 function getAllSettings(settings) {
-	const groupLabelAll = 'All DFP Settings';
+	const groupLabelAll = 'All parameters';
 	let code = `console.groupCollapsed('${groupLabelAll}');`;
 
 	Object.keys(settings).forEach(key => {
@@ -116,4 +149,26 @@ function getAllSettings(settings) {
 	code += `console.groupEnd('${groupLabelAll}');`;
 
 	return code;
+}
+
+function getError(details, success) {
+	let errorDetails = '';
+
+	if (!success) {
+		let errorPairs = '';
+		Object.keys(details).forEach(key => {
+			errorPairs += `console.info('%c${key}: %c${details[key]}', 'font-weight: bold;', 'font-weight: normal;');`;
+		});
+		errorDetails += `${createLogGroup(`'Error details'`, `${errorPairs}`)}`;
+	}
+
+	return errorDetails;
+}
+
+function createLogGroup(label, content, collapsed = false) {
+	return `
+		console[${collapsed ? '\'groupCollapsed\'' : '\'group\''}](${label});
+		${content}
+		console.groupEnd(${label});
+	`;
 }
